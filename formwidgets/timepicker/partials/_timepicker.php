afaeb3c449ba09d36c8bb475ce28756654b211ef
tohurtv <joshwebb84@outlook.com>
2025-06-20 21:19:08 -0600

    timepicker update

--- formwidgets/timepicker/partials/_timepicker.php
+++ formwidgets/timepicker/partials/_timepicker.php
@@ -1,17 +1,12 @@
 <?php
-$parentIndex = $this->parentIndex ?? 0;
-$childIndex = $this->index ?? 0;
-$keyName = $field->fieldName; // NOT getName()
 $fieldId = $this->getId();
 $fieldValue = $field->value;
+$inputName = $field->getName();
 $attributes = $field->attributes ?? [];
-
-$inputName = "working_schedule[{$parentIndex}][time_blocks][{$childIndex}][{$keyName}]";
-$inputId = "{$fieldId}_{$parentIndex}_{$childIndex}";
 ?>
 <input
     type="time"
-    id="<?= e($inputId) ?>"
+    id="<?= e($fieldId) ?>"
     name="<?= e($inputName) ?>"
     value="<?= e($fieldValue) ?>"
     class="custom-timepicker"
@@ -19,4 +14,3 @@ $inputId = "{$fieldId}_{$parentIndex}_{$childIndex}";
         <?= e($attr) ?>="<?= e($val) ?>"
     <?php endforeach; ?>
 >
-
